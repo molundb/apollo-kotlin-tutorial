551855bcc0812a867e07345762658cb1abe6d6a6
Martin Lundberg <martin_o_lundberg@hotmail.com>
2024-02-16 10:02:16 +0100

    feat: fetch and display mission info

--- compose/start/app/src/main/java/com/example/rocketreserver/LaunchList.kt
+++ compose/start/app/src/main/java/com/example/rocketreserver/LaunchList.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun LaunchList(onLaunchClick: (launchId: String) -> Unit) {
@@ -47,17 +48,19 @@ private fun LaunchItem(launch: LaunchListQuery.Launch, onClick: (launchId: Strin
         modifier = Modifier.clickable { onClick(launch.id) },
         headlineText = {
             // Mission name
-            Text(text = "Launch ${launch.id}")
+            Text(text = launch.mission?.name ?: "")
         },
         supportingText = {
             // Site
-            Text(text = "Site...")
+            Text(text = launch.site ?: "")
         },
         leadingContent = {
             // Mission patch
-            Image(
+            AsyncImage(
                 modifier = Modifier.size(68.dp, 68.dp),
-                painter = painterResource(R.drawable.ic_placeholder),
+                model = launch.mission?.missionPatch,
+                placeholder = painterResource(R.drawable.ic_placeholder),
+                error = painterResource(R.drawable.ic_placeholder),
                 contentDescription = "Mission patch"
             )
         }
